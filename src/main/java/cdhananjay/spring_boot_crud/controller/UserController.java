0577c20ac0ab96f204e0d7cafe7177d54028a8ab
Dhananjay Chavan <219833325+cdhananjay@users.noreply.github.com>
2026-08-29 11:37:11 +0530

    create all crud routes

--- src/main/java/cdhananjay/spring_boot_crud/controller/UserController.java
+++ src/main/java/cdhananjay/spring_boot_crud/controller/UserController.java
@@ -4,10 +4,8 @@ import cdhananjay.spring_boot_crud.entity.User;
 import cdhananjay.spring_boot_crud.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
@@ -22,5 +20,31 @@ public class UserController {
     public ResponseEntity<String> createUser(@RequestBody User user) {
         userService.createUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body("User Created");
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        User user = userService.getUser(id);
+        if (user == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getAllUsers());
+    }
+
+    @PutMapping()
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+        User updatedUser = userService.updateUser(user);
+        if (updatedUser == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.status(HttpStatus.OK).body(updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+        boolean success = userService.deleteUser(id);
+        if (success) return ResponseEntity.status(HttpStatus.OK).body("user deleted");
+        else return ResponseEntity.notFound().build();
     }
 }
--- src/main/java/cdhananjay/spring_boot_crud/service/UserService.java
+++ src/main/java/cdhananjay/spring_boot_crud/service/UserService.java
@@ -4,6 +4,9 @@ import cdhananjay.spring_boot_crud.entity.User;
 import cdhananjay.spring_boot_crud.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UserService {
     private UserRepository userRepository;
@@ -14,5 +17,27 @@ public class UserService {
         // validations
         // ...
         userRepository.save(user);
+    }
+
+    public User getUser(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.orElse(null);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User updateUser(User user){
+        Optional<User> existingUser = userRepository.findById(user.getId());
+        if (existingUser.isEmpty()) return null;
+        userRepository.save(user);
+        return user;
+    }
+
+    public boolean deleteUser(Long id) {
+        if(!userRepository.existsById(id)) return false;
+        userRepository.deleteById(id);
+        return true;
     }
 }
